705453dc71aa3148857abcb32d1565cc98caff59
Tadas <tadas@tedo.lt>
2022-04-24 21:54:40 +0300

    amzius, pasako kada gimei

---
+++ Java-script-petro-pazaidimai/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+
+const gimMetai = 37;
+const pcYear = gimMetai + 1;
+const currentYear = 2022;
+const birthYear = currentYear - pcYear;
+console.log(`Tu Gimei, ${birthYear} metais`);
+
+

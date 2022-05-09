28bf7ffb324950b4dd2a382862c8918f1edd49ef
Tadas <tadas@tedo.lt>
2022-05-09 20:43:56 +0300

    nebaigta-ND

---
+++ geguzes-9/PetroND/javascript.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const cars = ["BMW", "VW", "Audi"];
+
+cars.forEach((mokausi, i) => console.log(i + "; " + mokausi));
+
+// 3 uzduotis
+
+const kursoNariai = [
+  "Andrejus Kolesnikovas",
+  "Daiva Urbonavičiūtė",
+  "Darius Stančaitis",
+  "Donatas Borkys",
+  "Edita Koliataitė",
+  "Edvardas Kazlauskas",
+  "Egidijus Voveris",
+  "Gabrielė Bartašiūtė",
+  "Gytis Rickevičius",
+  "Ieva Stanevičiūtė",
+  "Inga Astrauskaitė",
+  "Karolis Žemelis",
+  "Lina Matulaitienė",
+  "Nerijus Grigonis",
+  "Ronaldas Česonis",
+  "Tadas Kertenis",
+  "Viktorija Kazakevičiūtė",
+  "Žilvinas Blinovas",
+];
